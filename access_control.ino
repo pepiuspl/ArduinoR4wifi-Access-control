2b13c42543985c73575d464f0e2bd27fe6ae6a37
pepiuspl <133570646+pepiuspl@users.noreply.github.com>
2026-06-12 23:10:41 +0000

    watchdog timeout

--- access_control.ino
+++ access_control.ino
@@ -800,8 +800,7 @@ void handleOnlineInstallerServer() {
 } 
 
 void executeCloudSynchronization() { 
-  if (strlen(proxmox_log_server) < 4) return;
-  checkOtaStatusFromServer(); 
+  if (strlen(proxmox_log_server) < 4) return; 
   WiFiClient httpCheck; 
   httpCheck.setTimeout(250);
   if (!httpCheck.connect(proxmox_log_server, proxmox_log_port)) { 
@@ -854,6 +853,7 @@ void performLocalFirmwareUpdate() {
   updateDisplay("AKTUALIZACJA OTA", "Pobieranie pliku..."); 
   
   if (otaClient.connect("192.168.0.200", 3000)) {
+    otaClient.setTimeout(5000);
     otaClient.print("GET /api/lock/download-firmware HTTP/1.1\r\n");
     otaClient.print("Host: 192.168.0.200\r\n");
     otaClient.print("Connection: close\r\n\r\n");
@@ -888,9 +888,8 @@ void performLocalFirmwareUpdate() {
 void checkOtaStatusFromServer() {
   WiFiClient client;
   
-  // Łączymy się z Twoim serwerem Node.js na Proxmoxie
   if (client.connect("192.168.0.200", 3000)) {
-    // Wysyłamy proste zapytanie do nowej ścieżki
+    client.setTimeout(5000);
     client.print("GET /api/lock/ota-check HTTP/1.1\r\n");
     client.print("Host: 192.168.0.200\r\n");
     client.print("Connection: close\r\n\r\n");
@@ -1039,7 +1038,8 @@ void setup() {
   lastFrameTick = millis(); 
 } 
 
-void loop() { 
+void loop() {
+  WDT.refresh(); 
   if (millis() - lastFrameTick > 80) { 
     lastFrameTick = millis();
     globalAnimFrame++; 
@@ -1231,5 +1231,6 @@ void loop() {
     globalDisplayInfo = ""; 
     digitalWrite(LED_GREEN, LOW); 
     digitalWrite(LED_RED, LOW); 
-  } 
+  }
+  checkOtaStatusFromServer(); 
 }
