4b5eb4d9c96dccfaa9ed8f6d36a4381ffa23e99e
pepiuspl <133570646+pepiuspl@users.noreply.github.com>
2026-06-15 20:16:41 +0200

    mac address change

--- access_control.ino
+++ access_control.ino
@@ -161,11 +161,11 @@ String getMacAddressString() {
   uint8_t mac[6];
   WiFi.macAddress(mac);
   char macBuf[18];
-  sprintf(macBuf, "%02X:%02X:%02X:%02X:%02X:%02X", mac[5], mac[4], mac[3], mac[2], mac[1], mac[0]);
+  sprintf(macBuf, "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
   return String(macBuf);
 }
 
-// 🌟 ALGORYTMICZNE UNIKALNE HASŁO FABRYCZNE
+// ALGORYTMICZNE UNIKALNE HASŁO FABRYCZNE
 String getFactoryAdminPassword() {
   String mac = getMacAddressString();
   unsigned long hashNum = 0;
