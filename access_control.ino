8cd4a3587c7ee2993506507b6c1b41eb2025493d
pepiuspl <133570646+pepiuspl@users.noreply.github.com>
2026-06-29 17:36:50 +0200

    v3.0.0 Smart-Link

--- access_control.ino
+++ access_control.ino
@@ -17,7 +17,7 @@
 
 unsigned long lastOtaCheck = 0;
 const unsigned long otaInterval = 10000;
-const char* app_version = "v2.9.9";
+const char* app_version = "v3.0.0";
 
 struct User { 
   byte uid[4]; 
